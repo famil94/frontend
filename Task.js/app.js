3d017e07b2389d376fdcb1c56a664abdf1979776
famil94 <famil.020394@gmail.com>
2024-03-27 12:40:56 +0400

    file ne

---
+++ Task.js/app.js
@@ -0,0 +1,101 @@
+//task-01
+//console.log("My name is Famil")
+
+
+//task-02
+// const name1 = "Famil"
+// console.log(name1);
+
+//Task-03
+//person 
+//$add
+
+//Task-04
+// let bookPrice = 750;
+// let bookAmount =14;
+// let total=bookPrice*bookAmount;
+// console.log(total);
+// alert(total)
+
+//task-05
+
+// let kmhSpeed = 75;
+// let msSpeed ;
+// console.log(msSpeed=kmhSpeed*1000/3600)
+
+// Task-6
+// let user = prompt("dogum ilinizi yazin ")
+// let il;
+// console.log(il=user*365)
+
+
+// task-07
+// prompt
+// let num1 ;
+// let num2;
+// for(i=9;i<100;i++){
+//     console.log(num1=i*5)
+//     console.log(num2=i/3)
+// }
+
+
+// task-8
+// let firstName = "famil";
+// let lastName = "ibrahimli";
+// console.log( firstName ,'' + lastName + " Tanışlığımıza çox şadam!");
+
+// task-9
+
+
+
+
+
+// tas-10
+let fullName = "Murad Orucov"
+for(i=0; i<fullName.length; i++){
+    console.log(fullName[i].length);
+}
+
+
+
+// task-15
+// let num1 = 12345
+// let num2 = 12345
+// let total = num1 * num2 + ""
+// console.log(total);
+
+//task-21
+// let userName = prompt("ad,soyad, ata adi daxil et")
+// let fullName = userName.split("")
+// let user = fullName[0]
+// let surname = fullName[1][0]
+// let parent = fullName[2][0]
+// console.log("${user} ${surname} ${parent}") 
+
+//task-22
+// let menfeet
+// let deposit= Number(prompt("depositi daxil edin : "));
+// let meblegi= Number(prompt("Emanet meblegini daxil edin : "));
+// let muddet = Number(prompt("emanetin muddetini daxil edin : "))
+// let faiz =Number(prompt("emanetin faiz derecesini daxil edin : "))
+
+// console.log(menfeet=deposit*faiz/100/muddet)
+
+
+// console.log(deposit)
+
+
+// task-20
+// let salary = prompt("emek haqqinizi qeyd edin")
+// let vergisiz = salary - (salary-13/100)
+// console.log( vergi istisna olmaqla masiniz : $(vergisiz));
+
+// task-25
+// let num = prompt("3 dene 2 reqemli eded daxil edin").split(" ")
+// letnum1 = +num[0]
+// let num2 = + num[1]
+// let num3 = num[2]
+
+// if(num1>num2 && num1<num3 || num1<num2 && num1>num3){
+//     console.log((num1));
+// }else if(num2>num1 && num2<num3 || )
